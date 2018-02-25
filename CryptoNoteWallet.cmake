f9e2aba9c6eb31f1041c8bfa9162bfa17ac2456a
heroNi <36719910+heroNi@users.noreply.github.com>
2018-02-24 21:05:39 -0600

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "manekineko")
+set(CN_CURRENCY_DISPLAY_NAME "ManekiNeko")
+set(CN_CURRENCY_TICKER "MaNe")
